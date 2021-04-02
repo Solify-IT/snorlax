8f0b62b32fcca83043b8088489b8d01a113b4655
Topiltzin Hernández Mares <topiltzin.hernandez@wizeline.com>
2021-04-01 23:06:54 -0600

    chore(backend): change logger format

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,14 +10,21 @@ import { wrapError } from './@types';
 
 const app: express.Application = express();
 const logger = winston.createLogger({
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.json(),
-  ),
   transports: [
-    new winston.transports.Console(),
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp(),
+        winston.format.simple(),
+      ),
+    }),
     new winston.transports.File({
-      filename: 'logs/error/error.log', level: 'error',
+      filename: 'logs/error/error.log',
+      level: 'error',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
     }),
   ],
 });
