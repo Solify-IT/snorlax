ac74139067e45a8e47a4d9510575eee3389fd56e
Topiltzin Hernández Mares <topiltzin.hernandez@wizeline.com>
2021-04-09 14:11:34 -0500

    chore(frontend): add routes file

--- frontend/src/Components/Router/Router.tsx
+++ frontend/src/Components/Router/Router.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import RegisterForm from 'src/views/Books.RegisterForm';
+import { NEW_BOOK } from './routes';
 
 const Router: React.FC = () => (
   <BrowserRouter>
     <Switch>
       {/* {} es para ingresar código de JS o TS */}
-      <Route exact path="/libros/nuevo" component={() => <>Hola</>} />
+      <Route exact path={NEW_BOOK} component={RegisterForm} />
     </Switch>
   </BrowserRouter>
 );
