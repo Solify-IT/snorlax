458d7242bcac1d976fecfea954555b66dbd57750
Topiltzin Hernández Mares <topiltzin.hernandez@wizeline.com>
2021-04-12 17:20:27 -0500

    test(frontend): fix failing test

--- frontend/src/views/App/App.tsx
+++ frontend/src/views/App/App.tsx
@@ -5,7 +5,7 @@ import 'antd/dist/antd.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" data-testid="app">
       <BrowserRouter>
         <Router />
       </BrowserRouter>
