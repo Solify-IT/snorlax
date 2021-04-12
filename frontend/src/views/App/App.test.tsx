99cf07760c4399fa770a627a468b457246db2c15
KevinWoolfolk <42701464+KevinWoolfolk@users.noreply.github.com>
2021-04-11 22:42:18 -0700

    instalación
    instalación de librerias

--- frontend/src/views/App/App.test.tsx
+++ frontend/src/views/App/App.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import '@testing-library/jest-dom/extend-expect';
+
 
 test('renders cargó', () => {
   render(<App />);
