42c7774862b0937979b080df24d185e98c2fce9f
a01702321 <a01702321@itesm.mx>
2021-04-28 13:31:24 -0500

    feat:create migration

---
+++ backend/src/infrastructure/datastore/migrations/1619634279415_add-type-movement.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import { MigrationBuilder, ColumnDefinitions } from 'node-pg-migrate';
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+    //TODO: Define type
+    pgm.addColumn('movements',{
+        type: {
+            type: 'any',
+            notNull: true,
+        }
+    })
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {
+    pgm.dropColumn('movements', 'type', {
+        cascade: true,
+    })
+}
