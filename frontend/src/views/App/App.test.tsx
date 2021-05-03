6ef2518891905b067b5a0e967dafb024f2e1293f
JuanCarlosCV <36903314+JuanCarlosCV@users.noreply.github.com>
2021-05-03 18:30:42 -0500

    fix(frontend) navigation

--- frontend/src/views/App/App.test.tsx
+++ frontend/src/views/App/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { NavigationContextProvider } from 'src/hooks/navigation';
 import App from './App';
 
 test('renders cargó', () => {
-  render(<App />);
+  render(
+    <NavigationContextProvider>
+      <App />
+    </NavigationContextProvider>,
+  );
   const appContainer = screen.getByTestId('app');
   expect(appContainer).toBeInTheDocument();
 });
