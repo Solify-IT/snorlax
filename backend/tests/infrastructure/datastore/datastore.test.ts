0c92d655f38413d6cc24580f4dfdc76fc3a1ded5
Topiltzin Hernández Mares <topiltzin.hernandez@wizeline.com>
2021-05-03 17:52:29 -0500

    test(datastore): add test cases for datastore

--- backend/tests/infrastructure/datastore/datastore.test.ts
+++ backend/tests/infrastructure/datastore/datastore.test.ts
@@ -2,7 +2,8 @@ import { Pool } from 'pg';
 import { wrapError } from 'src/@types';
 import { Library, LIBRARY_TABLE_NAME } from 'src/domain/model';
 import Datastore from 'src/infrastructure/datastore/datastore';
-import LibraryFactory from 'src/infrastructure/factories/libraryFactory';
+import LibraryFactory, { givenALibrary } from 'src/infrastructure/factories/libraryFactory';
+import NotFoundError from 'src/usecases/errors/notFoundError';
 import winston from 'winston';
 
 const errorLoggerFn = jest.fn();
@@ -39,5 +40,64 @@ describe('insert', () => {
 
     expect(error).toBe(null);
     expect(result).toBe(library.id);
+  });
+});
+
+describe('get', () => {
+  it('should return the found object when existing', async () => {
+    const LIBRARIES_LENGTH = 3;
+    await givenALibrary(
+      datastore, LibraryFactory.buildList(LIBRARIES_LENGTH),
+    );
+
+    const [result, error] = await wrapError(datastore.get(
+      `SELECT * FROM ${LIBRARY_TABLE_NAME}`,
+    ));
+
+    expect(error).toBe(null);
+    expect(result!.length).toBe(LIBRARIES_LENGTH);
+  });
+
+  it('should return an empty array when not found anything', async () => {
+    await givenALibrary(
+      datastore, LibraryFactory.buildList(3, { name: 'Testito' }),
+    );
+
+    const [result, error] = await wrapError(datastore.get(
+      `SELECT * FROM ${LIBRARY_TABLE_NAME} WHERE name = 'Testote'`,
+    ));
+
+    expect(error).toBe(null);
+    expect(result!.length).toBe(0);
+  });
+});
+
+describe('getById', () => {
+  it('should return the found object when existing', async () => {
+    const LIBRARIES_LENGTH = 3;
+    const [library] = await givenALibrary(
+      datastore, LibraryFactory.buildList(LIBRARIES_LENGTH),
+    );
+
+    const [result, error] = await wrapError(
+      datastore.getById<Library>(LIBRARY_TABLE_NAME, library.id),
+    );
+
+    expect(error).toBe(null);
+    expect(result!.id).toBe(library.id);
+  });
+
+  it('should throw an error when not found', async () => {
+    await givenALibrary(
+      datastore, LibraryFactory.buildList(3, { name: 'Testito' }),
+    );
+
+    const [result, error] = await wrapError(
+      datastore.getById(LIBRARY_TABLE_NAME, LibraryFactory.build().id),
+    );
+
+    expect(error).not.toBe(null);
+    expect(error).toBeInstanceOf(NotFoundError);
+    expect(result).toBeNull();
   });
 });
