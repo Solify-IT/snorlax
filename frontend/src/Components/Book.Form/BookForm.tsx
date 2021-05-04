c5cd8e6ca57ad721bffd21e8c1e29c7292eeb3f3
Topiltzin Hernández Mares <topiltzin.hernandez@wizeline.com>
2021-05-03 23:07:52 -0500

    fix(frontend): add missing fields to BookForm

--- frontend/src/Components/Book.Form/BookForm.tsx
+++ frontend/src/Components/Book.Form/BookForm.tsx
@@ -1,7 +1,7 @@
 import {
   Button, Form, Input, InputNumber, Switch,
 } from 'antd';
-import React from 'react';
+import React, { useState } from 'react';
 import Props from './BookForm.type';
 
 const layout = {
@@ -20,64 +20,188 @@ const BookForm: React.FC<Props> = ({
   form,
   onISBNChange,
   isLoading,
-}) => (
-  <Form
-    {...layout}
-    form={form}
-    name="registerBook"
-    initialValues={initialState}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    size="large"
-  >
-    <Form.Item
-      label="ISBN"
-      name="isbn"
-      rules={[{
-        required: true,
-        message: 'Debes ingresar un ISBN válido',
-        len: 13,
-      }]}
+}) => {
+  const [isManualInsert] = useState(false);
+
+  return (
+    <Form
+      {...layout}
+      form={form}
+      name="registerBook"
+      initialValues={initialState}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      size="large"
     >
-      <Input onChange={onISBNChange} />
-    </Form.Item>
-
-    <Form.Item
-      label="Precio"
-      name="price"
-      rules={[{
-        required: true,
-        message: 'Debes ingresar un precio válido',
-      }]}
-    >
-      <InputNumber />
-    </Form.Item>
-
-    <Form.Item
-      label="Cantidad"
-      name="amount"
-      rules={[{
-        required: true,
-        message: 'Debes ingresar una cantidad válida',
-      }]}
-    >
-      <InputNumber />
-    </Form.Item>
-
-    <Form.Item label="¿Es consigna?" name="isLoan">
-      <Switch />
-    </Form.Item>
-
-    <Form.Item {...tailLayout}>
-      <Button
-        loading={isLoading}
-        type="primary"
-        htmlType="submit"
+      <Form.Item
+        label="ISBN"
+        name="isbn"
+        rules={[{
+          required: true,
+          message: 'Debes ingresar un ISBN válido',
+          len: 13,
+        }]}
       >
-        Guardar
-      </Button>
-    </Form.Item>
-  </Form>
-);
+        <Input onChange={onISBNChange} />
+      </Form.Item>
+
+      <Form.Item
+        label="Precio"
+        name="price"
+        rules={[{
+          required: true,
+          message: 'Debes ingresar un precio válido',
+        }]}
+      >
+        <InputNumber />
+      </Form.Item>
+
+      <Form.Item
+        label="Cantidad"
+        name="amount"
+        rules={[{
+          required: true,
+          message: 'Debes ingresar una cantidad válida',
+        }]}
+      >
+        <InputNumber />
+      </Form.Item>
+
+      <Form.Item label="¿Es consigna?" name="isLoan">
+        <Switch />
+      </Form.Item>
+
+      <Form.Item
+        label="Título"
+        name="title"
+        rules={[{
+          required: isManualInsert,
+          message: 'Debes ingresar un título',
+        }]}
+      >
+        <Input disabled={!isManualInsert} readOnly={!isManualInsert} onChange={onISBNChange} />
+      </Form.Item>
+
+      <Form.Item
+        label="Autor"
+        name="author"
+        rules={[{
+          required: isManualInsert,
+          message: 'Debes ingresar un autor',
+        }]}
+      >
+        <Input disabled={!isManualInsert} readOnly={!isManualInsert} />
+      </Form.Item>
+
+      <Form.Item
+        label="Sinopsis"
+        name="synopsis"
+      >
+        <Input.TextArea disabled={!isManualInsert} readOnly={!isManualInsert} />
+      </Form.Item>
+
+      <Form.Item
+        label="Páginas"
+        name="pages"
+      >
+        <InputNumber disabled={!isManualInsert} readOnly={!isManualInsert} />
+      </Form.Item>
+
+      <Form.Item
+        label="Costo unitario"
+        name="unitaryCost"
+      >
+        <InputNumber disabled={!isManualInsert} readOnly={!isManualInsert} />
+      </Form.Item>
+
+      <Form.Item
+        label="Area"
+        name="area"
+      >
+        <Input disabled={!isManualInsert} readOnly={!isManualInsert} />
+      </Form.Item>
+
+      <Form.Item
+        label="Encuadernación"
+        name="coverType"
+      >
+        <Input disabled={!isManualInsert} readOnly={!isManualInsert} />
+      </Form.Item>
+
+      <Form.Item
+        label="Colección"
+        name="collection"
+      >
+        <Input disabled={!isManualInsert} readOnly={!isManualInsert} />
+      </Form.Item>
+
+      <Form.Item
+        label="Distribuidor"
+        name="distribuitor"
+      >
+        <Input disabled={!isManualInsert} readOnly={!isManualInsert} />
+      </Form.Item>
+
+      <Form.Item
+        label="Editorial"
+        name="editorial"
+      >
+        <Input disabled={!isManualInsert} readOnly={!isManualInsert} />
+      </Form.Item>
+
+      <Form.Item
+        label="Proveedor"
+        name="provider"
+      >
+        <Input disabled={!isManualInsert} readOnly={!isManualInsert} />
+      </Form.Item>
+
+      <Form.Item
+        label="Subcategoría"
+        name="subCategory"
+      >
+        <Input disabled={!isManualInsert} readOnly={!isManualInsert} />
+      </Form.Item>
+
+      <Form.Item
+        label="Tema"
+        name="theme"
+      >
+        <Input disabled={!isManualInsert} readOnly={!isManualInsert} />
+      </Form.Item>
+
+      <Form.Item
+        label="Subtema"
+        name="subTheme"
+      >
+        <Input disabled={!isManualInsert} readOnly={!isManualInsert} />
+      </Form.Item>
+
+      <Form.Item
+        label="Tipo"
+        name="type"
+      >
+        <Input disabled={!isManualInsert} readOnly={!isManualInsert} />
+      </Form.Item>
+
+      <Form.Item
+        label="URL de portada"
+        name="coverImageUrl"
+      >
+        <Input disabled={!isManualInsert} readOnly={!isManualInsert} />
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button
+          loading={isLoading}
+          type="primary"
+          htmlType="submit"
+        >
+          Guardar
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
 
 export default BookForm;
