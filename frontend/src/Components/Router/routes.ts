58f1ff7e45613ee7bbd799a1732993c363f36877
JuanCarlosCV <36903314+JuanCarlosCV@users.noreply.github.com>
2021-05-03 19:34:07 -0500

    fix(Frontend) local variables

--- frontend/src/Components/Router/routes.ts
+++ frontend/src/Components/Router/routes.ts
@@ -7,7 +7,6 @@ export const ADMIN = '/admin';
 export const BOOKS = `${INVENTORY}/libros`;
 export const NEW_BOOK = `${BOOKS}/nuevo`;
 export const BOOK_DETAIL = `${BOOKS}/:id`;
-export const LIST_LOCAL_BOOKS = `${BOOKS}/lista-local`;
 export const LIST_LOCAL_BOOKS = `${BOOKS}/buscar`;
 export const toBookDetail = (
   uuid: string,
