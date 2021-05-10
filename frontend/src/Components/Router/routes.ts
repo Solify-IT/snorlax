50fe619fa819877450b889db02aaafb3ef34bbcd
JuanCarlosCV <36903314+JuanCarlosCV@users.noreply.github.com>
2021-05-09 22:58:19 -0500

    fix comments frontend

--- frontend/src/Components/Router/routes.ts
+++ frontend/src/Components/Router/routes.ts
@@ -7,12 +7,15 @@ export const ADMIN = '/admin';
 export const BOOKS = `${INVENTORY}/libros`;
 export const NEW_BOOK = `${BOOKS}/nuevo`;
 export const BOOK_DETAIL = `${BOOKS}/:id`;
+
 export const LIST_LOCAL_BOOKS = `${BOOKS}/buscar`;
+
 export const toBookDetail = (
   uuid: string,
 ) => `${BOOKS}/${uuid}`;
 
 export const LIBRARIES = `${ADMIN}/librerías`;
+
 export const toLibraryDetail = (
   uuid: string,
 ) => `${LIBRARIES}/${uuid}`;
@@ -20,6 +23,7 @@ export const toLibraryDetail = (
 export const USERS = `${ADMIN}/usuarios`;
 export const LIST_USERS = `${USERS}/lista-local`;
 export const NEW_USER = `${USERS}/nuevo`;
+
 export const toUserDetail = (
   uuid: string,
 ) => `${USERS}/${uuid}`;
