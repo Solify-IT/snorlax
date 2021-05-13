44d601d8cb642c063b16244df1d81af318331774
Topiltzin Hernández Mares <topiltzin.hernandez@wizeline.com>
2021-05-13 17:19:20 -0500

    fix(backend): change identation size

--- backend/src/infrastructure/router/authMiddleware.ts
+++ backend/src/infrastructure/router/authMiddleware.ts
@@ -1,33 +1,34 @@
-import {NextFunction, Request, Response} from 'express';
+import { NextFunction, Request, Response } from 'express';
 import firebase from 'firebase-admin';
 import IUserRepository from 'src/usecases/repository/userRepository';
 
-const authMiddleware = (userRepository: IUserRepository) => async (req: Request, res: Response, next: NextFunction ) => {
-    const token = req.headers.authorization; 
+const authMiddleware = (
+  userRepository: IUserRepository,
+) => async (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization;
 
-    if(!token){
-        res.sendStatus(401);
-        return;
-    }
+  if (!token) {
+    res.sendStatus(401);
+    return;
+  }
 
-    const decoded = await firebase.auth().verifyIdToken(token);
+  const decoded = await firebase.auth().verifyIdToken(token);
 
-    if (!decoded.email) {
-        res.sendStatus(401);
-        return;
-    }
-  
-    const user = await userRepository.findOneOrNullByEmail(decoded.email);
-  
-    if (!user) {
-        res.sendStatus(401);
-        return;
-    }
+  if (!decoded.email) {
+    res.sendStatus(401);
+    return;
+  }
 
-    req.currentUser = user;
-    
-    next();
+  const user = await userRepository.findOneOrNullByEmail(decoded.email);
+
+  if (!user) {
+    res.sendStatus(401);
+    return;
+  }
+
+  req.currentUser = user;
+
+  next();
 };
 
 export default authMiddleware;
-
