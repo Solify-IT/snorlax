cc15fa3d265a0707c3e908e5b8119fed76b2dd83
Topiltzin Hernández Mares <topiltzin.hernandez@wizeline.com>
2021-05-26 16:54:43 -0500

    feat(frontend): search books to buy

--- frontend/src/views/ShoppingCart/ShoppingCart.compound.tsx
+++ frontend/src/views/ShoppingCart/ShoppingCart.compound.tsx
@@ -1,15 +1,19 @@
 import { notification } from 'antd';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Redirect } from 'react-router-dom';
+import { Book } from 'src/@types';
 import { SIGN_IN } from 'src/Components/Router/routes';
 import useAuth from 'src/hooks/auth';
 import useNavigation from 'src/hooks/navigation';
+import { useBackend } from 'src/integrations/backend';
 import { SHOW_SHOPPING_CART } from 'src/utils/featureToggles';
 import ShoppingCartComp from './ShoppingCart';
 
 const ShoppingCart: React.FC = () => {
   const { user, getHomeForRole } = useAuth();
   const { setTitles } = useNavigation();
+  const [books, setBooks] = useState<{ book: Book, amount: number }[]>();
+  const backend = useBackend();
 
   useEffect(() => {
     if (!user || !SHOW_SHOPPING_CART) return;
@@ -29,7 +33,37 @@ const ShoppingCart: React.FC = () => {
     return <Redirect to={getHomeForRole(user.role.name)} />;
   }
 
-  return (<ShoppingCartComp user={user} />);
+  const fetchBook = async (isbn: string) => {
+    if (isbn.length !== 13) {
+      notification.warning({ message: 'El ISBN no es válido' });
+      return;
+    }
+
+    const [res, err] = await backend.books.getAll<{
+      total: number,
+      books: Book[],
+    }>(`libraryId=${user.libraryId}&isbn=${isbn}`);
+
+    if (err || !res || (res.status !== 404 && res.status !== 200)) {
+      notification.error({ message: '¡Error al obtener el libro!' });
+      return;
+    }
+
+    if (res.status === 404 || res.data.books.length !== 1) {
+      notification.warn({ message: '¡No se encontró el libro!' });
+      return;
+    }
+
+    const newBook = { book: res.data.books[0], amount: 1 };
+    console.log(newBook);
+    if (books) {
+      setBooks([...books, newBook]);
+      return;
+    }
+    setBooks([newBook]);
+  };
+
+  return (<ShoppingCartComp books={books || []} fetchBook={fetchBook} />);
 };
 
 export default ShoppingCart;
--- frontend/src/views/ShoppingCart/ShoppingCart.tsx
+++ frontend/src/views/ShoppingCart/ShoppingCart.tsx
@@ -1,18 +1,120 @@
-// import Search from 'antd/lib/input/Search';
-import React from 'react';
-import { AuthUserType } from 'src/hooks/auth';
+import {
+  InputNumber, Table, Tag, Typography,
+} from 'antd';
+import Search from 'antd/lib/input/Search';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { Book } from 'src/@types';
+import { toBookDetail } from 'src/Components/Router/routes';
+import formatISBN from 'src/utils/isbn';
+
+type ShoppingCartBook = { book: Book, amount: number };
 
 interface Props {
-  user: AuthUserType;
+  books: ShoppingCartBook[];
+  fetchBook(isbn: string): void;
 }
 
-const ShoppingCart: React.FC<Props> = ({ user }) => (
-  <>
-    Carrito de compras del usuario con email
-    {` ${user.email}`}
-    de la librería
-    {` ${user.library.name}`}
-  </>
-);
+const ShoppingCart: React.FC<Props> = ({ books, fetchBook }) => {
+  const [isLoading] = useState(false);
+  const history = useHistory();
+
+  const goTo = (path: string) => () => history.push(path);
+
+  const columns = [
+    {
+      title: 'Title',
+      dataIndex: ['book', 'title'],
+      key: 'title',
+      render: (title: string, row: ShoppingCartBook) => (
+        <Link to={toBookDetail(row.book.id)}>{title}</Link>
+      ),
+    },
+    {
+      title: 'ISBN',
+      dataIndex: ['book', 'isbn'],
+      key: 'isbn',
+      render: (isbn: string) => <Typography.Text>{formatISBN(isbn)}</Typography.Text>,
+      width: 200,
+    },
+    {
+      title: 'Precio',
+      dataIndex: ['book', 'price'],
+      key: 'price',
+      render: (price: string) => (
+        <Typography.Text>
+          $
+          {price}
+        </Typography.Text>
+      ),
+      width: 100,
+    },
+    {
+      title: 'Autores',
+      dataIndex: ['book', 'author'],
+      key: 'author',
+      render: (author?: string) => (
+        <>
+          {author && author.split(', ').map(
+            (a) => <Tag key={a}>{a}</Tag>,
+          )}
+        </>
+      ),
+    },
+    {
+      title: 'Disponibilidad',
+      dataIndex: ['book', 'price'],
+      key: 'price',
+      render: (price: string) => (
+        <Typography.Text>
+
+          {price}
+          pz
+        </Typography.Text>
+      ),
+      width: 100,
+    },
+    {
+      title: 'Cantidad',
+      dataIndex: '',
+      key: 'view',
+      render: (row: ShoppingCartBook) => (
+        <InputNumber value={row.amount} />
+      ),
+    },
+  ];
+
+  const onSearch = (value?: string) => value && fetchBook(value);
+
+  const onSearchChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const newVal = e.target.value;
+    if (newVal.length !== 13) {
+      return;
+    }
+
+    fetchBook(newVal);
+  };
+
+  return (
+    <>
+      <Search
+        allowClear
+        enterButton="Añadir"
+        size="large"
+        placeholder="Ingresa el ISBN del libro a vender"
+        onSearch={onSearch}
+        loading={isLoading}
+        onChange={onSearchChange}
+        style={{ margin: '12px 0 24px 0' }}
+      />
+
+      <Table
+        loading={isLoading}
+        dataSource={books}
+        columns={columns}
+      />
+    </>
+  );
+};
 
 export default ShoppingCart;
