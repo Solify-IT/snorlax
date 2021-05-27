1c283600418acb92c813ffee7a765df4c92224bf
javier.iniguez <javier.iniguez2498@gmail.com>
2021-05-27 16:45:13 -0500

    feat: SNLX-16: Show view of recovery view

--- frontend/src/views/Users.ForgotPassword/ForgotPassword.tsx
+++ frontend/src/views/Users.ForgotPassword/ForgotPassword.tsx
@@ -1,18 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { Redirect, useHistory } from 'react-router-dom';
-import {
-  Form, Input, Button, Row, Col, notification, Alert, Checkbox,
-} from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
-import { wrapError } from 'src/@types';
-import useAuth from 'src/hooks/auth';
-import useFirebase from 'src/hooks/firebase';
+import React, { useEffect } from 'react';
 import useNavigation from 'src/hooks/navigation';
-import { useBackend } from 'src/integrations/backend';
-import Firebase from 'src/integrations/firebase/firebase';
-import styles from './ForgotPassword.styles.module.css';
+// import { Redirect, useHistory } from 'react-router-dom';
+import {
+  Form, Input, Button,
+} from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+// import { wrapError } from 'src/@types';
+// import useAuth from 'src/hooks/auth';
+// import useFirebase from 'src/hooks/firebase';
+// import useNavigation from 'src/hooks/navigation';
+// import { useBackend } from 'src/integrations/backend';
+// import Firebase from 'src/integrations/firebase/firebase';
+// import styles from './ForgotPassword.styles.module.css';
 
-const ForgotPassword = () => {
+const ForgotPassword: React.FC = () => {
+  const { setTitles } = useNavigation();
+  useEffect(() => {
+    setTitles({
+      title: 'Recover Password',
+    });
+  }, [setTitles]);
+
   const onFinish = (values: any) => {
     console.log('Received values of form: ', values);
   };
@@ -30,23 +38,11 @@ const ForgotPassword = () => {
       >
         <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
       </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[{ required: true, message: 'Please input your Password!' }]}
-      >
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="Password"
-        />
-      </Form.Item>
       <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
       </Form.Item>
-
     </Form>
   );
 };
