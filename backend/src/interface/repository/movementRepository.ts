deb04cd92e7c9c8f1bdf64418207625fdb4d3a4f
JuanCarlosCV <36903314+JuanCarlosCV@users.noreply.github.com>
2021-06-05 14:04:59 -0500

    feat(backend) see movements controller

--- backend/src/interface/repository/movementRepository.ts
+++ backend/src/interface/repository/movementRepository.ts
@@ -1,9 +1,21 @@
-import { Movement, MovementInputData, MOVEMENT_TABLE_NAME } from 'src/domain/model';
+import {
+  Movement, MovementInputData, MOVEMENT_TABLE_NAME, BOOK_TABLE_NAME,
+} from 'src/domain/model';
 import { IMovementRepository } from 'src/usecases';
 import { v4 as uuidv4 } from 'uuid';
 import BaseRepository from './BaseRepository';
 
 export default class MovementRepository extends BaseRepository implements IMovementRepository {
+  listAllMovements(): Promise<Movement[]> {
+    return this.datastore.get(`SELECT to_char(${MOVEMENT_TABLE_NAME}.created_at,'YYYY-MM-DD HH-MI-SS') as fecha ,${MOVEMENT_TABLE_NAME}.typ
+     count(${MOVEMENT_TABLE_NAME}.created_at) as total_count,
+      sum(${BOOK_TABLE_NAME}.price) as total
+    FROM ${MOVEMENT_TABLE_NAME} ,${BOOK_TABLE_NAME} 
+    WHERE ${BOOK_TABLE_NAME}.id = ${MOVEMENT_TABLE_NAME}.local_book_id
+    GROUP BY fecha,typ
+    ORDER BY fecha`);
+  }
+
   async registerMovement(movementData: MovementInputData): Promise<Movement['id']> {
     const id = uuidv4();
 
