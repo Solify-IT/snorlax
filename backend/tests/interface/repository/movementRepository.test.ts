c52788433372a3cb19f6dfe277a0a36d6c65727d
KevinWoolfolk <42701464+KevinWoolfolk@users.noreply.github.com>
2021-06-05 23:20:45 -0500

    fix: movement repository

--- backend/tests/interface/repository/movementRepository.test.ts
+++ backend/tests/interface/repository/movementRepository.test.ts
@@ -31,7 +31,7 @@ describe('registerMovement', () => {
     );
 
     const [result, error] = await wrapError(repository.registerMovement({
-      amount: 10, localBookId: localBook.id, isLoan: false, type: 'Devolucion Editorial',
+      amount: 10, localBookId: localBook.id, isLoan: false, total: 0, type: 'Devolucion Editorial',
     }));
 
     expect(error).toBe(null);
